96a36d33bd150a54895098009c9c668d1a21c9ab
DmitriyPudlo <teinalandergor@gmail.com>
2022-07-03 00:19:57 +0500

    0.8

--- models.py
+++ models.py
@@ -1,16 +1,15 @@
-from sqlalchemy import create_engine, Column, Integer, String, Interval, MetaData, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Interval, MetaData, ForeignKey, REAL
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
+metadata = MetaData()
 
 
 class Publisher(Base):
     __tablename__ = 'publisher'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    books = relationship("Book")
 
 
 class Book(Base):
@@ -18,15 +17,12 @@ class Book(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     id_publisher = Column(Integer, ForeignKey('publisher.id'))
-    publisher = relationship("Publisher")
-    stock = relationship('Stock')
 
 
 class Shop(Base):
     __tablename__ = 'shop'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    stock = relationship('Stock')
 
 
 class Stock(Base):
@@ -34,23 +30,22 @@ class Stock(Base):
     id = Column(Integer, primary_key=True)
     count = Column(Integer, nullable=False)
     id_book = Column(Integer, ForeignKey('book.id'))
-    books = relationship('Book')
     id_shop = Column(Integer, ForeignKey('shop.id'))
-    shop = relationship('Shop')
-    sale = relationship('Sale')
 
 
 class Sale(Base):
     __tablename__ = 'sale'
     id = Column(Integer, primary_key=True)
-    price = Column(Integer, nullable=False)
-    date_sale = Column(Interval, nullable=False)
+    price = Column(REAL, nullable=False)
+    count = Column(Integer, nullable=False)
+    date_sale = Column(String(100), nullable=False)
     id_stock = Column(Integer, ForeignKey('stock.id'))
-    stock = relationship('Stock')
 
 
-if __name__ == '__main__':
-    metadata = MetaData()
+def creat_tables():
     engine = create_engine("postgresql+psycopg2://postgres:3616@localhost/postgres", echo=True)
     engine.connect()
     Base.metadata.create_all(engine)
+
+
+creat_tables()
